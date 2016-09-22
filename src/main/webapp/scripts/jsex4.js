21d9483e6ac627a688974fc96fa57329d7c9c51a
Patrick Johansen <Patrick Johansen>
2016-09-22 16:22:30 +0200

    Solved js exercise 4 part 1, 3 & 4

---
+++ src/main/webapp/scripts/jsex4.js
@@ -0,0 +1,79 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+
+    callEveryHour();
+
+    $("#btn").click(function () {
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
+            $("#quoteOfTheHour").html((a[0].content + "<p>— " + a[0].title + "</p>"));
+        });
+
+        /* Doesn't work. Don't know how to use .load(). I tried:
+         $("#QuoteOfTheHour").load("http://api.icndb.com/jokes/random", function (responseTxt, statusTxt, xhr) {
+         if (statusTxt === "success") {
+         alert("External content loaded successfully!");
+         }
+         if (statusTxt === "error") {
+         alert("Error: " + xhr.status + ": " + xhr.statusText);
+         }
+         });
+         */
+
+    });
+    
+    (function updateTime() {
+            $.ajax({
+                url: 'http://localhost:8080/module2js/time',
+                type: 'GET',
+                dataType: 'json',
+                success: function (res) {
+                    $('#time').html(res.hour + ":" + res.minute + ":" + res.second);
+                },
+                error: function (res) {
+                    console.log("Error");
+                },
+                complete: function () {
+                    setTimeout(updateTime, 1000);
+                }
+            })
+            ;
+        })();
+    
+    $("#north").click(function() {
+        alert("You clicked north");
+    });
+    
+    $("#south").click(function() {
+        alert("You clicked south");
+    });
+    
+    $("#west").click(function() {
+        alert("You clicked west");
+    });
+    
+    $("#east").click(function() {
+        alert("You clicked east");
+    });
+    
+});
+
+function callEveryHour() {
+    setInterval(loadQuote, 1000 * 60 * 60);
+}
+
+function testCall() {
+    setInterval(loadQuote, 1 * 1 * 1);
+}
+
+function loadQuote() {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
+        $("#quoteOfTheHour").html((a[0].content + "<p>— " + a[0].title + "</p>"));
+    });
+}
+
+
+
